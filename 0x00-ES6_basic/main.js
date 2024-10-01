cc82d06c3116367f4d0e5253f083e6d010c1703c
Your Name <you@example.com>
2024-10-01 03:02:53 +0100

    practiced rest operator

---
+++ 0x00-ES6_basic/main.js
@@ -0,0 +1,11 @@
+// Arrow functions
+// import getNeighborhoodsList from './2-arrow.js';
+// const neighborhoodsList = new getNeighborhoodsList();
+// const res = neighborhoodsList.addNeighborhood('Noe Valley');
+// console.log(res);
+//  Default Parameters
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
