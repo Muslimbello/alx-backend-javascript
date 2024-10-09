7358990a59581b6dd859b3038e11cbad16191e64
Your Name <you@example.com>
2024-10-09 03:40:33 +0100

    finishing taask 4 and 5

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -6,7 +6,17 @@
 
 // console.log(getFullResponseFromAPI(true));
 // console.log(getFullResponseFromAPI(false));
-import handleResponseFromAPI from "./2-then.js";
+// import handleResponseFromAPI from "./2-then.js";
 
-const promise = Promise.reject();
-handleResponseFromAPI(promise);
+// const promise = Promise.reject();
+// handleResponseFromAPI(promise);
+
+// import handleProfileSignup from "./3-all";
+
+// handleProfileSignup();
+// import signUpUser from "./4-user-promise";
+
+// console.log(signUpUser("Bob", "Dylan"));
+import uploadPhoto from "./5-photo-reject";
+
+console.log(uploadPhoto("guillaume.jpg"));
